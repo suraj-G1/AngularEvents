87dd40d22308ed268b602246ee239d6c8b1de7c5
Suraj Gund <suraj@dotohealth.com>
2025-10-13 07:59:19 +0530

    add events in angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,13 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular-events');
+
+  handleEvent(event: Event) {
+    console.log('Type of the Event :', event.type);
+    console.log("Value",(event.target as HTMLInputElement).value)
+  }
 }
